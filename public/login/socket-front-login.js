5b688351308b949e336a39946bbf260f84358d6c
LuisFelipeA <felipeadriani2@gmail.com>
2024-10-30 20:49:16 -0300

    Autenticando login usuario

---
+++ public/login/socket-front-login.js
@@ -0,0 +1,16 @@
+const socket = io();
+
+function emitirAutenticarUsuario (dados) {
+    socket.emit("autenticar_usuario", dados);
+}
+
+socket.on("autenticacao_sucesso", () => {
+    alert("Autenticado com sucesso")
+    window.location.href = "/";
+});
+
+socket.on("autenticacao_erro", () => alert("Senha errada"));
+
+socket.on("usuario_nao_encontrado", () => {alert("Usuario não encontrado")});
+
+export {emitirAutenticarUsuario};
--- servidor/socket-back.js
+++ servidor/socket-back.js
@@ -1,10 +1,12 @@
 import registrarEventosDocumento from "./registrarEventos/documento.js";
 import registrarEventosInicio from "./registrarEventos/inicio.js";
 import registrarEventosCadastro from "./registrarEventos/cadastros.js";
+import registrarEventosLogin from "./registrarEventos/login.js";
 import io from "./servidor.js";
 
 io.on("connection", (socket) => {
   registrarEventosInicio(socket, io);
   registrarEventosDocumento(socket, io);
   registrarEventosCadastro(socket, io);
+  registrarEventosLogin(socket, io);
 });
